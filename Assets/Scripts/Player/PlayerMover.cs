4a2e5fb6ec27df9e20978e025d8f2e9b3279868a
ayato0913a <ayato0913a@gmail.com>
2026-03-03 14:28:42 +0900

    [ fix ] コメント追加

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
+/// <summary>
+///     プレイヤーの移動制御をするクラス
+/// </summary>
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
 
     private Rigidbody2D _rb;
 
+    /// <summary>
+    ///     移動処理を行う
+    /// </summary>
+    /// <param name="xInput"></param>
     public void Move(float xInput)
     {
         _rb.linearVelocity = new Vector2(_moveSpeed * xInput, 0f);
     }
 
+    /// <summary>
+    ///     強制停止する
+    /// </summary>
     public void Stop()
     {
         _rb.linearVelocity = Vector2.zero;
