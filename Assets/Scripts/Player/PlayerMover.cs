5dde598a239de72427bc1de371cdfc44073a8864
ayato0913a <ayato0913a@gmail.com>
2026-03-03 14:18:27 +0900

    [ feat ] プレイヤーの移動処理するクラスを作成

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -2,4 +2,22 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody2D _rb;
+
+    public void Move(float xInput)
+    {
+        _rb.linearVelocity = new Vector2(_moveSpeed * xInput, 0f);
+    }
+
+    public void Stop()
+    {
+        _rb.linearVelocity = Vector2.zero;
+    }
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 }
