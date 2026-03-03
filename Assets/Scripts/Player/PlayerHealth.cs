79be599d18c51ab8670f8cb0c2864244d54e1777
ayato0913a <ayato0913a@gmail.com>
2026-03-03 11:14:08 +0900

    [ feat ] プレイヤーの体力管理クラスを作成

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+/// <summary>
+///     プレイヤーの体力を管理するクラス
+/// </summary>
+public class PlayerHealth : MonoBehaviour,IDamageable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Health")]
+    [SerializeField, ReadOnly] private int _currentHealth;
+    [SerializeField] private int _maxHealth;
+
+    public void Die()
     {
-        
+        Debug.Log(" プレイヤー死亡 ");
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
+        if (_currentHealth < damage)
+        {
+            _currentHealth = 0;
+        }
+        else
+        {
+            _currentHealth -= damage;
+        }
+        Debug.Log($" プレイヤーに{damage}ダメージ、プレイヤーの現在のHP{_currentHealth}");
         
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
     }
 }
