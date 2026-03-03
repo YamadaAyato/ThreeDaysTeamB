5b258ea58bfe8f6d6cab4fd90a7e07f77eb21a61
ayato0913a <ayato0913a@gmail.com>
2026-03-03 23:16:06 +0900

    [ feat ] Flip用のクラスを追加

--- Assets/Scripts/Player/PlayerSwichFlip.cs
+++ Assets/Scripts/Player/PlayerSwichFlip.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerSwichFlip : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private SpriteRenderer _sprite;
+
+    public void SwichFlip(float xInput)
     {
-        
+        // 画像が初期が左向きなため、右に移動する場合はtrueにする
+        if (xInput > 0)
+        {
+            _sprite.flipX = true;
+        }
+        else if (xInput < 0)
+        {
+            _sprite.flipX = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _sprite = GetComponent<SpriteRenderer>();
     }
 }
