8c9f0f2e20748878e26c291f1efa40caf3ccd151
ayato0913a <ayato0913a@gmail.com>
2026-03-03 14:18:55 +0900

    [ feat ] プレイヤーの入力管理をするクラスを作成

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,9 +1,57 @@
 using UnityEngine;
 
-public class PlayerInputHandler
+/// <summary>
+///     プレイヤーの入力管理を行うクラス
+/// </summary>
+public class PlayerInputHandler : MonoBehaviour
 {
+    private PlayerMover _playerMover;
+    private PlayerStateMachine _playerStateMachine;
+
+    private float _xInput;
+
+    private void Awake()
+    {
+        _playerMover = GetComponent<PlayerMover>();
+        _playerStateMachine = new PlayerStateMachine();
+    }
+
     private void Update()
     {
+        // プレイヤーの状態がActionLockedの場合、入力を処理しない
+        if (_playerStateMachine.IsActionLocked) return;
 
+        // 水平方向の入力を取得
+        _xInput = Input.GetAxisRaw("Horizontal");
+
+        bool leftInput = Input.GetMouseButtonDown(0);
+        bool rightInput = Input.GetMouseButtonDown(1);
+
+        if(_playerMover != null)
+        {
+            if (_xInput != 0f)
+            {
+                _playerStateMachine.ChangeState(PlayerState.Move);
+            }
+            else
+            {
+                _playerMover.Stop();
+                _playerStateMachine.ChangeState(PlayerState.Idle);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_playerStateMachine.IsActionLocked)
+        {
+            _playerMover.Stop();
+            return;
+        }
+
+        if (_xInput != 0f)
+            _playerMover.Move(_xInput);
+        else
+            _playerMover.Stop();
     }
 }
