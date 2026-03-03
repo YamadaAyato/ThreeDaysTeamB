e6f9820143b49cff879a4f99ffdc68d120df203d
ayato0913a <ayato0913a@gmail.com>
2026-03-03 14:19:21 +0900

    [ feat ] プレイヤーの状態を管理するクラスを作成

--- Assets/Scripts/System/PlayerStateMachine.cs
+++ Assets/Scripts/System/PlayerStateMachine.cs
@@ -10,17 +10,26 @@ public enum PlayerState
     ActionLocked,
 }
 
-public class PlayerStateMachine : MonoBehaviour
+/// <summary>
+///     プレイヤーの状態を管理するステートマシン
+/// </summary>
+public class PlayerStateMachine
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    /// <summary> 現在のプレイヤーの状態を示すプロパティ </summary>
+    public PlayerState CurrentState => _currentState;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>  現在の状態がActionLockedかどうかを示すプロパティ </summary>
+    public bool IsActionLocked => _currentState == PlayerState.ActionLocked;
+
+    private PlayerState _currentState;
+
+    /// <summary>
+    ///     Stateを変更するメソッド
+    /// </summary>
+    /// <param name="newState"> 次のState </param>
+    public void ChangeState(PlayerState newState)
     {
-        
+        if(_currentState == newState) return;
+        _currentState = newState;
     }
 }
