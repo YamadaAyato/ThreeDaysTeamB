4c4dc6975e7fae911fdf951574b8bf24752585b3
ayato0913a <ayato0913a@gmail.com>
2026-03-04 00:59:11 +0900

    [ fix ] サマリつけた

--- Assets/Scripts/Player/PlayerSwichFlip.cs
+++ Assets/Scripts/Player/PlayerSwichFlip.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
+/// <summary>
+///     プレイヤーの向きを切り替えるクラス
+/// </summary>
 public class PlayerSwichFlip : MonoBehaviour
 {
     private SpriteRenderer _sprite;
 
+    /// <summary>
+    ///     向きを切り替えるメソッド
+    /// </summary>
+    /// <param name="xInput"></param>
     public void SwichFlip(float xInput)
     {
         // 画像が初期が左向きなため、右に移動する場合はtrueにする
