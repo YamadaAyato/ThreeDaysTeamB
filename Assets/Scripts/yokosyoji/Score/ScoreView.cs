909975c11ee461e08932b806dc6435ae18845e95
junyoko1 <junyoko1@outlook.jp>
2026-03-03 17:40:07 +0900

    [ fix ]　コメント追加

--- Assets/Scripts/yokosyoji/Score/ScoreView.cs
+++ Assets/Scripts/yokosyoji/Score/ScoreView.cs
@@ -1,12 +1,16 @@
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+///スコアUI反映
+/// </summary>
+
 public class ScoreView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
 
     public void UpdateScore(float score)
     {
-        scoreText.text="Score:" + score.ToString("00000.00");
+        scoreText.text="Score:" + score.ToString("00000");
     }
 }
