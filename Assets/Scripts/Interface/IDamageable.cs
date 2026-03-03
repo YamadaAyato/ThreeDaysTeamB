1894ab26034d534ce1616bbc34c4bae299ddb276
ayato0913a <ayato0913a@gmail.com>
2026-03-03 10:26:45 +0900

    [ feat ] ダメージを受けるもののインターフェイスを作成

--- Assets/Scripts/Interface/IDamageable.cs
+++ Assets/Scripts/Interface/IDamageable.cs
@@ -1,6 +1,10 @@
-using UnityEngine;
-
-public class IDamageable
+/// <summary> ダメージ関係の処理をする </summary>
+public interface IDamageable
 {
-    
+    /// <summary> ダメージ処理をする </summary>
+    /// <param name="damage"> ダメージ </param>
+    void TakeDamage(int damage);
+
+    /// <summary> 死亡時処理をする </summary>
+    void Die();
 }
