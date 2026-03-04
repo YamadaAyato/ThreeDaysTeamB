c7591e7ba0fb58622542574b32c0de43aa505ef0
ayato0913a <ayato0913a@gmail.com>
2026-03-04 11:28:08 +0900

    [ fix ] コメント追加

--- Assets/Scripts/Effects/TitleManager.cs
+++ Assets/Scripts/Effects/TitleManager.cs
@@ -1,11 +1,17 @@
 using DG.Tweening;
 using UnityEngine;
 
+/// <summary>
+///     タイトル状態での管理を行うクラス
+/// </summary>
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private float _fadeDuration;
 
+    /// <summary>
+    ///     ゲーム開始ボタンがクリックされたときの処理
+    /// </summary>
     public void GameStartClick()
     {
         _canvasGroup.DOFade(0, _fadeDuration)
@@ -14,6 +20,9 @@ public class TitleManager : MonoBehaviour
             );
     }
 
+    /// <summary>
+    ///     ゲーム終了ボタンがクリックされたときの処理
+    /// </summary>
     public void GameExitClick()
     {
         Application.Quit();
