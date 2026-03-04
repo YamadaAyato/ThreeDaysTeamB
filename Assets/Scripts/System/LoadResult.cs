90d50db84cedaccc7e1096f38d8dbfa663dd7180
ayato0913a <ayato0913a@gmail.com>
2026-03-05 06:57:04 +0900

    [ fix ] リザルトをLoadするものを追加

--- Assets/Scripts/System/LoadResult.cs
+++ Assets/Scripts/System/LoadResult.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class LoadResult : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void EndInGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneLoader.LoadScene("Result");
     }
 }
