5937cedb7d60107ce7df4b7ffa4f70035410ef49
ayato0913a <ayato0913a@gmail.com>
2026-03-04 11:18:31 +0900

    [ feat ] タイトル用のcanvasをfadeする機能を作成

--- Assets/Scripts/Effects/TitleManager.cs
+++ Assets/Scripts/Effects/TitleManager.cs
@@ -1,7 +1,21 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup canvasGroup;
-    [SerializeField] private float fadeDuration;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private float _fadeDuration;
+
+    public void Click()
+    {
+        _canvasGroup.DOFade(0, _fadeDuration)
+            .OnComplete(() 
+            => StartGame()
+            );
+    }
+
+    private void StartGame()
+    {
+        //TODO : プレイヤーが召喚されるタイムラインを呼び出すとおもう
+    }
 }
