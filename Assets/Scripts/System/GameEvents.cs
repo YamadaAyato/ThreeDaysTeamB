7dd58b667ff11c37458a81ac58a2e56b90263c9d
ayato0913a <ayato0913a@gmail.com>
2026-03-04 10:47:03 +0900

    [ feat ] GameStartのイベントを作成

--- Assets/Scripts/System/GameEvents.cs
+++ Assets/Scripts/System/GameEvents.cs
@@ -3,5 +3,10 @@ using System;
 
 public static class GameEvents
 {
+    public static event Action OnGameStart;
 
+    public static void GameStart()
+    {
+        OnGameStart?.Invoke();
+    }
 }
