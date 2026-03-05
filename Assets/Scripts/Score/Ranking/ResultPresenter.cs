01b4fe38ecdb2ba797a6da4a8173df7cd62e0613
junyoko1 <junyoko1@outlook.jp>
2026-03-05 11:31:27 +0900

    リザルト画面作成

--- Assets/Scripts/Score/Ranking/ResultPresenter.cs
+++ Assets/Scripts/Score/Ranking/ResultPresenter.cs
@@ -5,8 +5,8 @@ public class ResultPresenter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _rankText;
     [SerializeField] private TextMeshProUGUI _scoreText;
-    [SerializeField] private TextMeshProUGUI _formulaText; // 時間×倒した数用
-
+    [SerializeField] private TextMeshProUGUI _enemyCountText;
+    [SerializeField] private TextMeshProUGUI _timeText;
     private void Start()
     {
         int myRank = PlayerPrefs.GetInt("MyRank", -1);
@@ -21,7 +21,8 @@ public class ResultPresenter : MonoBehaviour
         PlayerPrefs.Save();
 
         _rankText.text = myRank == -1 ? "圏外" : $"{myRank}位";
-        _scoreText.text = $"{myScore:00000}";
-        _formulaText.text = $"{myTime}秒 × {myCount}体";
+        _scoreText.text = $"{ myScore:000000}";
+        _enemyCountText.text = $"{myCount}体";
+        _timeText.text= $"{myCount}体";
     }
 }
